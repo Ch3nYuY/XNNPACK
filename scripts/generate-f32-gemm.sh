dd040f42f14f0b3f900370d8dc50186f82ba07aa
Chenyu Yang <chenyu.yang@intel.com>
2024-08-29 00:33:07 +0800

    add f32 gemm 5x16 u2/4/8/16

--- scripts/generate-f32-gemm.sh
+++ scripts/generate-f32-gemm.sh
@@ -580,6 +580,13 @@ tools/xngen src/f32-gemm/avx-broadcast.c.in -D MR=3 -D NR=16 -D AVX=1 -D FMA=3 -
 tools/xngen src/f32-gemm/avx-broadcast.c.in -D MR=4 -D NR=16 -D AVX=1 -D FMA=3 -D INC=0 -D DATATYPE=F32 -o src/f32-gemm/gen/f32-gemm-4x16-minmax-fma3-broadcast.c &
 tools/xngen src/f32-gemm/avx-broadcast.c.in -D MR=4 -D NR=16 -D AVX=1 -D FMA=3 -D INC=1 -D DATATYPE=F32 -o src/f32-gemminc/gen/f32-gemminc-4x16-minmax-fma3-broadcast.c &
 tools/xngen src/f32-gemm/avx-broadcast.c.in -D MR=5 -D NR=16 -D AVX=1 -D FMA=3 -D INC=0 -D DATATYPE=F32 -o src/f32-gemm/gen/f32-gemm-5x16-minmax-fma3-broadcast.c &
+# f32 tuning
+tools/xngen src/f32-gemm/avx-broadcast-chenyu.c.in -D MR=5 -D NR=16 -D AVX=1 -D FMA=3 -D INC=0 -D DATATYPE=F32 -D UNROLL=2 -o src/f32-gemm/gen/f32-gemm-5x16-minmax-fma3-u2-broadcast.c &
+tools/xngen src/f32-gemm/avx-broadcast-chenyu.c.in -D MR=5 -D NR=16 -D AVX=1 -D FMA=3 -D INC=0 -D DATATYPE=F32 -D UNROLL=4 -o src/f32-gemm/gen/f32-gemm-5x16-minmax-fma3-u4-broadcast.c &
+tools/xngen src/f32-gemm/avx-broadcast-chenyu.c.in -D MR=5 -D NR=16 -D AVX=1 -D FMA=3 -D INC=0 -D DATATYPE=F32 -D UNROLL=8 -o src/f32-gemm/gen/f32-gemm-5x16-minmax-fma3-u8-broadcast.c &
+tools/xngen src/f32-gemm/avx-broadcast-chenyu.c.in -D MR=5 -D NR=16 -D AVX=1 -D FMA=3 -D INC=0 -D DATATYPE=F32 -D UNROLL=16 -o src/f32-gemm/gen/f32-gemm-5x16-minmax-fma3-u16-broadcast.c &
+
+
 tools/xngen src/f32-gemm/avx-broadcast.c.in -D MR=5 -D NR=16 -D AVX=1 -D FMA=3 -D INC=1 -D DATATYPE=F32 -o src/f32-gemminc/gen/f32-gemminc-5x16-minmax-fma3-broadcast.c &
 tools/xngen src/f32-gemm/avx-broadcast.c.in -D MR=6 -D NR=16 -D AVX=1 -D FMA=3 -D INC=0 -D DATATYPE=F32 -o src/f32-gemm/gen/f32-gemm-6x16-minmax-fma3-broadcast.c &
 tools/xngen src/f32-gemm/avx-broadcast.c.in -D MR=6 -D NR=16 -D AVX=1 -D FMA=3 -D INC=1 -D DATATYPE=F32 -o src/f32-gemminc/gen/f32-gemminc-6x16-minmax-fma3-broadcast.c &
